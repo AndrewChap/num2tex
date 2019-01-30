6eee36d243d57901af240b2bf1f3ada690803a86
Andrew Chap <31779927+AndrewChap@users.noreply.github.com>
2019-01-30 13:17:15 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='num2tex',
+      version='0.1',
+      description='Convert float or int to a TeX-formatted string',
+      url='http://github.com/AndrewChap/num2tex',
+      author='Andrew Chap',
+      author_email='andrew@andrewchap.com',
+      license='MIT',
+      packages=['num2tex'],
+      zip_safe=False)
