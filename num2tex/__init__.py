3cbef3997862793e4eca5cc62415d0a6c6686021
Andrew Chap <31779927+AndrewChap@users.noreply.github.com>
2019-02-01 12:17:14 -0700

    changing the num2tex function to a class
    num2tex is now a class that inherits from str

--- num2tex/__init__.py
+++ num2tex/__init__.py
@@ -1,8 +1,47 @@
-# from https://stackoverflow.com/questions/13490292/format-number-using-latex-notation-in-python/13490601#13490601
-def num2tex(f):
-    float_str = "{0:.2g}".format(f)
-    if "e" in float_str:
-        base, exponent = float_str.split("e")
-        return r"{0} \times 10^{{{1}}}".format(base, int(exponent))
-    else:
-        return float_str
+import math
+
+class num2tex(str):
+    def __new__(self, num, precision=None):
+        self.num = num
+        self.precision = precision
+        return str.__new__(self,num)
+
+    # __format__() gets called, for example,
+    # when evaluating 'a={0.2g}'.format(num2tex(2.5456))
+    def __format__(self,f):
+        return self.__str__(f)
+
+    # method that converts the number into a string
+    # with precision given by f (of type string)
+    # or by self.precision (of type int)
+    def __str__(self,f=None):
+        if self.num is None:
+            return '\mathrm{None}'
+        elif self.num == math.inf:
+            return '\infty'
+        elif self.num == -math.inf:
+            return '-\infty'
+        elif self.num != self.num:
+            return '\mathrm{NaN}'
+        if f is None and self.precision is None:
+            num_string = str(self.num)
+        else:
+            if f is None:
+                f = '0.{}'.format(self.precision)
+            num_string = "{0:{1}}".format(self.num,f)
+        if "e" in num_string:
+            base, exponent = num_string.split("e")
+            return r"{0} \times 10^{{{1}}}".format(base, int(exponent))
+        else:
+            return num_string
+    
+    # unambiguous string representation of num2tex object
+    def __repr__(self):
+        if self.precision is None:
+            return 'num2tex(num={})'.format(self.num)
+        else:
+            return 'num2tex(num={}, precision={})'.format(self.num,self.precision)
+    
+    # for pretty printing in a Jupyter Notebook
+    def _repr_latex_(self):
+        return '$$' + self.__str__() + '$$'
