16d609af0f3d5efb116b9adad028a3c0d1d363c1
Andrew Chap <andrew@andrewchap.com>
2019-02-08 14:19:25 -0700

    Adding in global options with a configure() function.  This allows different formatting of the multiplier, like using \cdot instead of \times, or the option of weather to display the unary factor in front of the exponential (i.e. whether to display  '1 \times 10^{23}' or just '10^{23}'

--- num2tex/__init__.py
+++ num2tex/__init__.py
@@ -1,36 +1,55 @@
+# Default options
+option_exp_format = '\\times 10^{}'
+option_display_singleton = False
+
+# Function to set options
+def configure(exp_format=None, display_singleton=None):
+    global option_exp_format
+    if exp_format is not None:
+        option_exp_format = exp_format
+    if display_singleton is not None:
+        option_display_singleton = display_singleton
+
 class num2tex(str):
-    def __new__(self, num, precision=None):
+    def __new__(self, num, precision=None, format_spec=None):
         self.num = num
         self.precision = precision
+        self.format_spec = format_spec
         return str.__new__(self,num)
 
     # __format__() gets called, for example,
     # when evaluating 'a={0.2g}'.format(num2tex(2.5456))
-    def __format__(self,f):
-        return self.__str__(f)
+    def __format__(self,format_spec):
+        return self.__str__(format_spec)
 
     # method that converts the number into a string
     # with precision given by f (of type string)
     # or by self.precision (of type int)
-    def __str__(self,f=None):
+    def __str__(self,format_spec=None):
+        if format_spec is None:
+            format_spec = self.format_spec
         if self.num is None:
-            return '\mathrm{None}'
+            return '\\mathrm{None}'
         elif self.num == float('inf'):
-            return '\infty'
+            return '\\infty'
         elif self.num == -float('inf'):
-            return '-\infty'
+            return '-\\infty'
         elif self.num != self.num:
-            return '\mathrm{NaN}'
-        formatUnspecified = (f is None) or (f == '')
+            return '\\mathrm{NaN}'
+        formatUnspecified = (format_spec is None) or (format_spec == '')
         if formatUnspecified and self.precision is None:
             num_string = str(self.num)
         else:
             if formatUnspecified:
-                f = '0.{}'.format(self.precision)
-            num_string = '{0:{1}}'.format(self.num,f)
+                format_spec = '0.{}'.format(self.precision)
+            num_string = '{0:{1}}'.format(self.num,format_spec)
         if 'e' in num_string:
             base, exponent = num_string.split('e')
-            return r'{0} \times 10^{{{1}}}'.format(base, int(exponent))
+            exp = (option_exp_format.format('{{{}}}')).format(int(exponent))
+            if base == '1' and option_display_singleton is False:
+                return r'{0}'.format(exp)
+            else:
+                return r'{0} {1}'.format(base,exp)
         else:
             return num_string
 
